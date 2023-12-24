10f1326ac169b1892747463539c251a588dd9910
dziubus466 <123731127+dziubus466@users.noreply.github.com>
2023-12-25 00:46:30 +0100

    Add files via upload

--- script2.js
+++ script2.js
@@ -2,20 +2,21 @@ let next= document.getElementById('next');
 let previous = document.getElementById('previous');
 let thanksText = document.getElementById('thanksText')
 const text = [
-"Wszystkigo najleszego z okazji Świąt Borzego narodzenia. ",
- "Chciałbym tobie podziękować za wspólnie spędzony czas w tym roku.",   
-"Dziękuje za każdą minutę którą mi pościeciałaś. Każda sekunda spędzona z tobą była najlepiej sporzytkowanym czasem w moim życiu.Jesteś dla mnie najważniejszą osobą, staram sie byc dla ciebie jak najlespzy.", 
-"Oczywiście zadażały się podknięcia i błędy przez co chwilami nie było wesolo lecz wspólnym siłami rozwiązaliśmy wszystie kłopoty.",
-"Dziękje że starasz się dla mnie oraz że spędzasz ze mna setki godzin na romowach.",
-"Dziekuje że zawsze jak cie potrzebuje to jesteś", 
-"Dziękuje że wytrzymałaś razem ze mną tak dlugo", 
-"Dziękuje za to że nadal mnie kochasz po duzej ilości błędów jakie popełniłem", 
-"Dziekuje za ten cudowny rok, mam nadzieje że kolejny spędzimy jeszcze bliżej siebie i lepiej.",
-"Kocham cię najbardziej. Fifi. "]
+    "Z okazji Świąt Bożego Narodzenia życzę Ci wszystkiego najlepszego.",
+    "Chciałbym Ci serdecznie podziękować za wspólnie spędzony czas w tym roku.",
+    "Dziękuję za każdą minutę, którą ze mną spędziłaś. Każda sekunda spędzona z Tobą była najlepiej spożytkowanym czasem w moim życiu. Jesteś dla mnie najważniejszą osobą, i staram się być dla Ciebie jak najlepszy.",
+    "Oczywiście zdarzały się podknięcia i błędy, przez co chwilami nie było wesoło, lecz wspólnymi siłami rozwiązaliśmy wszystkie kłopoty.",
+    "Dziękuję, że starasz się dla mnie oraz że spędzasz ze mną setki godzin na rozmowach.",
+    "Dziękuję, że zawsze, gdy Cię potrzebuję, jesteś przy mnie.",
+    "Dziękuję, że wytrzymałaś ze mną tak długo.",
+    "Dziękuję za to, że nadal mnie kochasz pomimo dużej ilości błędów, jakie popełniłem.",
+    "Dziękuję za ten cudowny rok. Mam nadzieję, że kolejny spędzimy jeszcze bliżej siebie i lepiej.",
+    "Kocham Cię najbardziej. Fifi.",
+    "Przepraszam, że nie umiem pisać."]
 let num = 0;
 next.addEventListener("click",function(){
     console.log("next")
-    if (num < 9){
+    if (num < 10){
         num+=1
     }
     thanksText.innerText=text[num]
